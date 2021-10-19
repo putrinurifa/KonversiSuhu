8978424a770815e417080a9e63badda44a2ddeae
putrinurifa <putrinurifa818@gmail.com>
2021-10-19 13:11:39 +0700

    chapter3

---
+++ lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'input.dart';
+import 'result.dart';
+import 'convert.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController inputSuhuController = TextEditingController();
+
+  double _inputUser = 0;
+  double _result = 0;
+
+  String _newValue = "Kelvin";
+
+  var listItem = ["Kelvin", "Reamur"];
+
+  List<String> listViewItem = <String>[];
+
+  void _temperatureConversion() {
+    setState(() {
+      _inputUser = double.parse(inputSuhuController.text);
+
+      if (_newValue == "Kelvin") {
+        _result = _inputUser + 273;
+        listViewItem.add("Kelvin: $_result");
+      } else {
+        _result = (4 / 5) * _inputUser;
+        listViewItem.add("Reamur: $_result");
+      }
+    });
+  }
+
+  void dropdownOnChanged(String? changeValue) {
+    setState(() {
+      _newValue = changeValue!;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Konversi Suhu'),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(8),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Input(inputSuhuController: inputSuhuController),
+              Container(
+                margin: EdgeInsets.only(top: 16),
+                width: 256,
+                child: DropdownButton<String>(
+                  isExpanded: true,
+                  items: listItem.map((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(
+                        value,
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                  value: _newValue,
+                  onChanged: dropdownOnChanged,
+                ),
+              ),
+              Result(
+                result: _result,
+              ),
+              Convert(
+                convertHandler: _temperatureConversion,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/result.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Result extends StatelessWidget {
+  const Result({Key? key, required this.result}) : super(key: key);
+
+  final double result;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 24, bottom: 24),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Hasil",
+            style: TextStyle(
+              fontSize: 24,
+            ),
+          ),
+          Text(
+            result.toStringAsFixed(1),
+            style: TextStyle(
+              fontSize: 56,
+            ),
+          )
+        ],
+      )
+    );
+  }
+}
